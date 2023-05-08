e7788f6cac2b72c3ef5a85a3b5156b30e7b092b2
Takashi Isezaki <isezaki.takashi@home>
2023-05-08 15:39:53 +0900

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#sudo docker build . -t takashi5515/takashi_env:0.69
-sudo docker build --build-arg http_proxy=http://proxy.rdh.ecl.ntt.co.jp:20066 --build-arg https_proxy=http://proxy.rdh.ecl.ntt.co.jp:20066 . -t takashi5515/takashi_env:0.81
+sudo docker build . -t takashi5515/takashi_env:0.81
+#sudo docker build --build-arg http_proxy=http://proxy.rdh.ecl.ntt.co.jp:20066 --build-arg https_proxy=http://proxy.rdh.ecl.ntt.co.jp:20066 . -t takashi5515/takashi_env:0.81
